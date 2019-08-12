8262466cf15d1285ae19c11bc478ea868a44867f
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-08-12 19:29:07 +0100

    Added global config variable REACT_APP_ENDPOINT Added submit button and form wrapping to LoginScreen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,35 +1,54 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 import ValidInput from '../components/ui/ValidInput';
 
 const LoginScreen = () => {
 
-    const loginHandler = async () => {
-        //await AsyncStorage.setItem('userToken', 'abc');
-        this.props.navigation.navigate('Main');
-      }
-      
-      const signupHandler = () => {
-        this.props.navigation.navigate('Signup');
-      }
+  const loginHandler = async () => {
+    //await AsyncStorage.setItem('userToken', 'abc');
+    this.props.navigation.navigate('Main');
+  }
 
-      return (
-        <div>
-          <h1>S K 8 M 8</h1>
-          <ValidInput
-            label='username'
-            validationrules={{
-              isUsername: true,
-            }}
-          />
-          <ValidInput
-              label='password'
-              validationrules={{
-                isPassword: true
-              }}
-          />
-        </div>
-      );
+  const signupHandler = () => {
+    this.props.navigation.navigate('Signup');
+  }
+
+  return (
+    <div>
+      <h1>S K 8 M 8</h1>
+      <form>
+        <ValidInput
+          label='username'
+          validationrules={{
+            isUsername: true,
+          }}
+        />
+        <ValidInput
+          label='password'
+          validationrules={{
+            isPassword: true
+          }}
+        />
+        <input
+          accept="image/*"
+          style={{display: 'none'}}
+          id="outlined-button-submit"
+          multiple
+          type="submit"
+        />
+        <label htmlFor="outlined-button-file">
+          <Button
+            variant="outlined"
+            color="primary"
+            component="span"
+          >
+            Log in
+          </Button>
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default LoginScreen;

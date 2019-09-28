1ba5672f33026d5f18b85af3df2650692333e3d5
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-09-28 12:53:42 +0100

    refactor: decided to use axios to as http client, added conditional param stuff for queryurlbuilder

--- src/utils/fetch/queryUrlBuilder.js
+++ src/utils/fetch/queryUrlBuilder.js
@@ -1,6 +1,7 @@
 
 const queryUrlBuilder = (endpoint, controller, action, params) => {
-    const queryParams = new URLSearchParams(params).toString();
+    let queryParams
+    params ? queryParams = new URLSearchParams(params).toString() : '';
     return `${endpoint}/${controller}/${action}/${queryParams}`
 };
 

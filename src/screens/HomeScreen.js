eb6d1e2ad4e44b6170589612d650a8311319bda8
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-09-29 11:46:43 +0100

    improvement: added account settings expansion panel to homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import '../styles/screens.css'
 
 const HomeScreen = () => {
 
+    const [expanded, setExpanded] = React.useState(false);
+
+    const togglePanel = panel => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
     return (
         <div className={'screen'}>
-            HomeScreen
+            <ExpansionPanel expanded={expanded === 'accountPanel'} onChange={togglePanel('panel1')}>
+                <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="account-settings"
+                    id="account-settings-header"
+                >
+                    <Typography>Account settings</Typography>
+                    <Typography>Manage your account details</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
         </div>
     );
 }

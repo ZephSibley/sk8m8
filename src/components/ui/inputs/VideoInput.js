9828df697614424c44470bb720b25571c0ddb42d
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-10-20 19:17:57 +0100

    refactor: VideoInput; we functional now. Got rid of a whole bunch of unnecessary junk. We also now show the name of the file uploaded

--- src/components/ui/inputs/VideoInput.js
+++ src/components/ui/inputs/VideoInput.js
@@ -1,69 +1,57 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
 import VideoLibraryOutlinedIcon from '@material-ui/icons/VideoLibraryOutlined';
 import FormHelperText from '@material-ui/core/FormHelperText';
 
 
-class VideoInput extends Component {
-    constructor(props) {
-        super(props);
-        this.handleVideoChange = this.handleVideoChange.bind(this);
-    }
+const VideoInput = props => {
+    const [fileName, setFileName] = useState('');
 
-    state = {
-        file: undefined,
-    };
-
-    handleVideoChange(e) {
+    const handleVideoChange = e => {
         e.preventDefault();
         let reader = new FileReader();
         let file = e.target.files[0];
         if (file) {
-            reader.onloadend = () => {
-                this.setState({
-                    file: file,
-                    imagePreviewUrl: reader.result
-                });
-            };
+            reader.onloadend = () => setFileName(file.name);
             reader.readAsDataURL(file);
-            this.props.setFieldValue(this.props.field.name, file);
+            props.setFieldValue(props.field.name, file);
         }
     }
 
-
-    render() {
-        const { errorMessage, title } = this.props;
-        const { name } = this.props.field;
-
-        return (
-            <FormControl margin='normal'>
-                <input
-                    style={{ display: "none" }}
-                    id='video-upload'
-                    name={name}
-                    type="file"
-                    accept="video/*"
-                    onChange={this.handleVideoChange}
-                />
-                <label htmlFor="video-upload">
-                    <Button
-                        color='primary'
-                        margin='normal'
-                        component="span"
-                    >
-                        {title}
-                        <VideoLibraryOutlinedIcon />
-                    </Button>
-                </label>
-                {errorMessage ? (
-                    <FormHelperText id="video-upload-helper-text" error={true}>
-                        {errorMessage}
-                    </FormHelperText>
-                ) : null}
-            </FormControl>
-        );
-    }
+    return (
+        <FormControl margin='normal'>
+            <input
+                style={{ display: "none" }}
+                id='video-upload'
+                name={props.field.name}
+                type="file"
+                accept="video/*"
+                onChange={handleVideoChange}
+            />
+            <label htmlFor="video-upload">
+                <Button
+                    color='primary'
+                    margin='normal'
+                    component="span"
+                >
+                    {props.title}
+                    <VideoLibraryOutlinedIcon />
+                </Button>
+            </label>
+            {props.errorMessage ? (
+                <FormHelperText id="video-upload-helper-text" error={true}>
+                    {props.errorMessage}
+                </FormHelperText>
+            ) : null}
+            {fileName ? (
+                <FormHelperText id="video-upload-filename">
+                    {fileName}
+                </FormHelperText>
+            ) : null}
+        </FormControl>
+    );
 }
+
 
 export default VideoInput;

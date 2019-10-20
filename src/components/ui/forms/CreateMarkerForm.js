c87164b52f33c0bba34cd1423060ee5cc15f9809
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-10-20 18:40:56 +0100

    refactor bugfix: Created a component to handle video file input, fixed the bug with it not working. The component started off as someone else's, probably refactor in the future to be functional. I already trimmed a bunch of fat.

--- src/components/ui/forms/CreateMarkerForm.js
+++ src/components/ui/forms/CreateMarkerForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Formik } from 'formik';
+import { Formik, Field } from 'formik';
 import *  as Yup from 'yup';
 import TextField from '@material-ui/core/TextField';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -9,7 +9,9 @@ import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
 
 import fetchLocationTypes from '../../../utils/map/fetchLocationTypes';
+import VideoInput from '../inputs/VideoInput';
 import formStyles from '../../../styles/forms';
+
 
 const CreateMarkerForm = props => {
     // prop: location; array, latlng
@@ -41,119 +43,105 @@ const CreateMarkerForm = props => {
             .min(3, "min. 3 characters"),
         location_type: Yup.string()
             .required("What kind of location are you marking?"),
-        marker_video: Yup.object().shape({
-            size: Yup.number()
-                .min(100, "We need a video!")
-                .max(262144000, "Sorry, your video is too big :("),
-            // name: Yup.string()
-            //     .required('We need a video!')
-        })
+        marker_video: Yup.mixed()
+            .required("We need a video!")
+            .test(
+                "fileSize",
+                "Your video is too big :(",
+                value => value && value.size <= 262144000
+            )
     })
-    
+
     return (
         <div>
-        <h4 className={formClasses.embeddedFormHeader}>
+            <h4 className={formClasses.embeddedFormHeader}>
                 Make your mark
         </h4>
-        <Formik
-            initialValues={{
-                marker_name: '',
-                location_type: '',
-                marker_video: '',
-                location: props.location,
-            }}
-            validationSchema={validationSchema}
-            onSubmit={(values, { setSubmitting }) => {
-                console.log(values);
-                setSubmitting(true);
-            }}
+            <Formik
+                initialValues={{
+                    marker_name: '',
+                    location_type: '',
+                    marker_video: null,
+                    location: props.location,
+                }}
+                validationSchema={validationSchema}
+                onSubmit={(values, { setSubmitting }) => {
+                    console.log(values);
+                    setSubmitting(true);
+                }}
 
-            render={({
-                values,
-                errors,
-                touched,
-                handleChange,
-                setFieldValue,
-                handleBlur,
-                isValid,
-                handleSubmit,
-                isSubmitting,
-            }) => (
-                    <form
-                        onSubmit={handleSubmit}
-                        className={formClasses.form}
-                    >
-                        <TextField
-                            name='marker_name'
-                            label='name'
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.marker_name}
-                            margin='normal'
-                            error={errors.marker_name && touched.marker_name}
-                            helperText={
-                                errors.marker_name && touched.marker_name ?
-                                    errors.marker_name : ''
-                            }
-                        />
-                        <FormControl margin='normal' style={{ maxWidth: 200 }}>
-                            <InputLabel htmlFor="location_type">Location Type</InputLabel>
-                            <Select
-                                native
-                                inputProps={{ name: 'location_type', id: 'location_type' }}
-                                style={{ width: 200, }}
-                                value={values.location_type}
+                render={({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    setFieldValue,
+                    handleBlur,
+                    isValid,
+                    handleSubmit,
+                    isSubmitting,
+                }) => (
+                        <form
+                            onSubmit={handleSubmit}
+                            className={formClasses.form}
+                        >
+                            <TextField
+                                name='marker_name'
+                                label='name'
                                 onChange={handleChange}
                                 onBlur={handleBlur}
-                            >
-                                {locationTypes}
-                            </Select>
-                            {errors.location_type &&
-                                touched.location_type &&
-                                <FormHelperText id="location_type_helper_text">
-                                    {errors.location_type}
-                                </FormHelperText>
-                            }
-                        </FormControl>
-                        <input
-                            style={{ display: 'none' }}
-                            accept="video/*"
-                            id="marker_video"
-                            name="marker_video"
-                            type="file"
-                            onChange={(event) => {
-                                setFieldValue("marker_video", event.currentTarget.files[0]);
-                            }}
-                        />
-                        <label htmlFor="marker_video">
+                                value={values.marker_name}
+                                margin='normal'
+                                error={errors.marker_name && touched.marker_name}
+                                helperText={
+                                    errors.marker_name && touched.marker_name ?
+                                        errors.marker_name : ''
+                                }
+                            />
+                            <FormControl margin='normal' style={{ maxWidth: 200 }}>
+                                <InputLabel htmlFor="location_type">Location Type</InputLabel>
+                                <Select
+                                    native
+                                    inputProps={{ name: 'location_type', id: 'location_type' }}
+                                    style={{ width: 200, }}
+                                    value={values.location_type}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                >
+                                    {locationTypes}
+                                </Select>
+                                {errors.location_type &&
+                                    touched.location_type &&
+                                    <FormHelperText id="location_type_helper_text">
+                                        {errors.location_type}
+                                    </FormHelperText>
+                                }
+                            </FormControl>
+                            <Field
+                                name="marker_video"
+                                component={VideoInput}
+                                title="Upload"
+                                setFieldValue={setFieldValue}
+                                errorMessage={errors.marker_video ?
+                                    errors.marker_video : ''
+                                }
+                                touched={touched.marker_video}
+                                onBlur={handleBlur}
+                            />
                             <Button
-                                variant="outlined"
-                                component="span"
-                                color="secondary"
+                                variant="contained"
+                                color="primary"
+                                component="button"
+                                type="submit"
+                                margin='normal'
                                 className={formClasses.submit}
+                                disabled={isSubmitting || !isValid}
                             >
-                                Choose Video
-                            </Button>
-                            {errors.marker_video &&
-                                <FormHelperText id="marker_video_helper_text">
-                                    {errors.marker_video && errors.marker_video.size}
-                                </FormHelperText>
-                            }
-                        </label>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            component="button"
-                            type="submit"
-                            margin='normal'
-                            className={formClasses.submit}
-                            disabled={isSubmitting || !isValid}
-                        >
-                            Create
+                                Create
                     </Button>
-                    </form>
-                )}
-        />
+                        </form>
+                    )}
+            />
         </div>
     )
 }

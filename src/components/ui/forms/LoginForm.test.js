4d849f3440245c69a94efce411a9716b6a608cea
Alice Marks <tug43760@temple.edu>
2019-11-13 17:11:03 -0500

    test: loginform

---
+++ src/components/ui/forms/LoginForm.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import LoginForm from './LoginForm';
+
+test('Basic Rendering', () => {
+    const component = renderer.create(
+        <LoginForm />
+    )
+    let tree = component.toJSON
+    expect(tree).toMatchSnapshot();
+})

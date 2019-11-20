cedc8939ac85b8f91ddee6cfb686a16fab4cd4a0
Alice Marks <tug43760@temple.edu>
2019-11-20 17:08:59 -0500

    adapted test to work with react router

--- src/screens/LoginScreen.test.js
+++ src/screens/LoginScreen.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginScreen from './LoginScreen';
 
 test('Basic Rendering', () => {
     const component = renderer.create(
-        <LoginScreen />
+        <Router>
+            <Route path='/' component={LoginScreen} />
+        </Router>
     )
     let tree = component.toJSON()
     expect(tree).toMatchSnapshot();

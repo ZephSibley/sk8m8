0d4035cc1159fc5b44d6a36653cae9de0eb7bf51
Alice Marks <tug43760@temple.edu>
2019-11-21 17:04:02 -0500

    tidying

--- src/components/account/UserCard.js
+++ src/components/account/UserCard.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardHeader from '@material-ui/core/CardHeader';

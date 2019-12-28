f7b92a4910da864b1a8d53ce9d17fe1870f27ecb
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-12-28 15:12:34 +0000

    refactor: Using hashhistory as routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+//import { hashHistory, browserHistory } from 'react-router';
 
 import standard from './styles/themes/standard';
 import PrivateRoute from './components/routing/PrivateRoute';

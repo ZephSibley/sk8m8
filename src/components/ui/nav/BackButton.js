91ac28c2418b1a252042308a90856d176778ffaa
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-12-29 15:03:17 +0000

    Implemented the simplest possible back button

---
+++ src/components/ui/nav/BackButton.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { makeStyles } from '@material-ui/core/styles';
+
+const BackButton = props => {
+    // prop: history; from withRouter
+
+    const useStyles = makeStyles({
+        root: {
+            //width: '100vw',
+            position: 'fixed',
+            top: 10,
+            left: 5,
+        },
+    });
+
+    const classes = useStyles();
+    return (
+        <ArrowBackIcon
+            className={classes.root}
+            onClick={() => window.history.back()}
+        />
+    )
+}
+
+export default BackButton;
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import '../styles/screens.css';
+import BackButton from '../components/ui/nav/BackButton';
 import SignupForm from '../components/ui/forms/SignupForm';
 
 const axios = require('axios');
@@ -9,6 +10,7 @@ const SignupScreen = () => {
 
     return (
         <div className={'form-screen'}>
+            <BackButton />
             <h1>S K 8 M 8</h1>
             <SignupForm requests={axios} />
         </div>

18a31761e6920f0e054e7346ee921c9731825277
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-12-29 15:14:07 +0000

    refactor: don't need props

--- src/components/ui/nav/BackButton.js
+++ src/components/ui/nav/BackButton.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { makeStyles } from '@material-ui/core/styles';
 
-const BackButton = props => {
-    // prop: history; from withRouter
+const BackButton = () => {
 
     const useStyles = makeStyles({
         root: {

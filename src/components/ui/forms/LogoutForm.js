cc65763ba659d04b588e7c303feb4c0ff58a2fb9
Zeph <zeph.sibley@yahoo.com>
2020-04-20 20:37:05 +0100

    refactor: logout form now just deletes JWT

--- src/components/ui/forms/LogoutForm.js
+++ src/components/ui/forms/LogoutForm.js
@@ -1,14 +1,9 @@
-import React, { useState, } from 'react';
+import React from 'react';
 import { Formik } from 'formik';
-import *  as Yup from 'yup';
 import Button from '@material-ui/core/Button';
-
-import formStyles from '../../../styles/forms';
-import BackendValidationError from '../text/BackendValidationError';
 
 
 const LogoutForm = ({ requests }) => {
-    const [submitError, setSubmitError] = useState([])
 
     return (
         <div style={{position: "absolute", right: 0}}>
@@ -16,28 +11,17 @@ const LogoutForm = ({ requests }) => {
                 
                 onSubmit={(values, { setSubmitting }) => {
                     setSubmitting(true);
-                    requests.get(
-                        `${process.env.REACT_APP_ENDPOINT}account/sitelogout`,
-                        { withCredentials: true }
-                    ).then(response => {
-                        window.localStorage.setItem('authenticated', false)
-                        window.location.href = "/"
-                    }).catch(err => {
-                        setSubmitting(false);
-                        setSubmitError(err.message || Object.values(JSON.parse(err)));
-                    })
+                    window.localStorage.removeItem('token')
+                    window.location.href = "/"
                 }}
 
                 render={({
-                    values,
                     handleSubmit,
                     isSubmitting,
-                    isValid,
                 }) => (
                         <form
                             onSubmit={handleSubmit}
                         >
-                            <BackendValidationError resp={submitError} />
                             <Button
                                 color="primary"
                                 component="button"

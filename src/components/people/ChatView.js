ec719353c73b1af5267037caeeb5b151b1f247a3
Zeph <zeph.sibley@yahoo.com>
2020-07-18 13:38:06 +0100

    Removed unused signalr lib, commented out chatview code for now

--- src/components/people/ChatView.js
+++ src/components/people/ChatView.js
@@ -1,85 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import * as signalR from '@aspnet/signalr';
-import List from '@material-ui/core/List';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import React from 'react';
+// import React, { useState, useEffect } from 'react';
+// import List from '@material-ui/core/List';
+// import TextField from '@material-ui/core/TextField';
+// import Button from '@material-ui/core/Button';
+// import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+// import CircularProgress from '@material-ui/core/CircularProgress';
 
-import MessageListItem from '../people/MessageListItem';
+// import MessageListItem from '../people/MessageListItem';
 
 const ChatView = props => {
     // Prop: interlocutor; string, person we're talking to
 
-    const [hubConnection, setHubConnection] = useState(null);
-    const [myMessage, updateMyMessage] = useState('');
-    const [messages, updateMessages] = useState([
-        <MessageListItem key={0} sender='' message='Loading' avatar={CircularProgress} />
-    ]);
+    // const [hubConnection, setHubConnection] = useState(null);
+    // const [myMessage, updateMyMessage] = useState('');
+    // const [messages, updateMessages] = useState([
+    //     <MessageListItem key={0} sender='' message='Loading' avatar={CircularProgress} />
+    // ]);
 
-    useEffect(() => {
-        setHubConnection(new signalR.HubConnectionBuilder()
-            .withUrl(`${process.env.REACT_APP_ENDPOINT}hubs/chat`, { /*accessTokenFactory: getAccessToken()*/ })
-            .configureLogging(signalR.LogLevel.Information)
-            .build()
-        );
-    }, []);
+    // useEffect(() => {
+    //     setHubConnection(new signalR.HubConnectionBuilder()
+    //         .withUrl(`${process.env.REACT_APP_ENDPOINT}hubs/chat`, { /*accessTokenFactory: getAccessToken()*/ })
+    //         .configureLogging(signalR.LogLevel.Information)
+    //         .build()
+    //     );
+    // }, []);
 
-    useEffect(() => {
-        if (hubConnection) {
-            hubConnection.start()
-                .then(() => console.log('Connected to chat'))
-                .catch(err => updateMessages([
-                    <MessageListItem key={0} sender='Uh oh D:' message={err} avatar={ErrorOutlineIcon} />
-                ]));
+    // useEffect(() => {
+    //     if (hubConnection) {
+    //         hubConnection.start()
+    //             .then(() => console.log('Connected to chat'))
+    //             .catch(err => updateMessages([
+    //                 <MessageListItem key={0} sender='Uh oh D:' message={err} avatar={ErrorOutlineIcon} />
+    //             ]));
             
-            // Might need updating
-            hubConnection.on('ReceiveMessage', (req) => {
-                updateMessages(m => [
-                    ...m,
-                    MessageListItem(req)
-                ])
-            })
-        }
-    }, [hubConnection])
+    //         // Might need updating
+    //         hubConnection.on('ReceiveMessage', (req) => {
+    //             updateMessages(m => [
+    //                 ...m,
+    //                 MessageListItem(req)
+    //             ])
+    //         })
+    //     }
+    // }, [hubConnection])
 
-    const sendMessage = async (interlocutor, message) => {
-        hubConnection
-            .invoke('SendMessage', interlocutor, message)
-            .catch(err => updateMessages([
-                ...messages,
-                <MessageListItem key={0} sender='Uh oh D:' message={err} avatar={ErrorOutlineIcon} />
-            ]));
-        updateMyMessage('');
-    }
+    // const sendMessage = async (interlocutor, message) => {
+    //     hubConnection
+    //         .invoke('SendMessage', interlocutor, message)
+    //         .catch(err => updateMessages([
+    //             ...messages,
+    //             <MessageListItem key={0} sender='Uh oh D:' message={err} avatar={ErrorOutlineIcon} />
+    //         ]));
+    //     updateMyMessage('');
+    // }
 
-    return(
-        <div className='screen'>
-            <List>
-                {messages}
-            </List>
-            <form autoComplete="off">
-                <TextField
-                    required
-                    id="outlined-dense-multiline"
-                    label="Dense multiline"
-                    margin="dense"
-                    variant="outlined"
-                    multiline
-                    rowsMax="20"
-                    onChange={e => updateMyMessage(e.target.value)}
-                />
-                <Button
-                    variant="outlined"
-                    color="primary"
-                    component="span"
-                    onClick={() => sendMessage(props.interlocutor, myMessage)}
-                >
-                    Send
-                </Button>
-            </form>
-        </div>
-    );
+    // return(
+    //     <div className='screen'>
+    //         <List>
+    //             {messages}
+    //         </List>
+    //         <form autoComplete="off">
+    //             <TextField
+    //                 required
+    //                 id="outlined-dense-multiline"
+    //                 label="Dense multiline"
+    //                 margin="dense"
+    //                 variant="outlined"
+    //                 multiline
+    //                 rowsMax="20"
+    //                 onChange={e => updateMyMessage(e.target.value)}
+    //             />
+    //             <Button
+    //                 variant="outlined"
+    //                 color="primary"
+    //                 component="span"
+    //                 onClick={() => sendMessage(props.interlocutor, myMessage)}
+    //             >
+    //                 Send
+    //             </Button>
+    //         </form>
+    //     </div>
+    // );
+    return <div></div>
 }
 
 export default ChatView
